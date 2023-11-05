f83e06b2f24ff8af8502df65da35285ce3ea465a
ankit yadav <83020828+ankit719@users.noreply.github.com>
2023-11-05 23:16:32 +0530

    This project contain HR database and Employee table and CRUD operation on it.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace HRDemo
+{
+    internal class Program
+    {
+        
+        private static IConfiguration _iconfiguration;
+        static void Main(string[] args)
+        {
+            GetAppSettingsFile();
+            EmpDisplay();
+        }
+        static void GetAppSettingsFile()
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("Appsettings.json", optional: false, reloadOnChange: true);
+            _iconfiguration = builder.Build();
+        }
+        static void EmpDisplay()
+        {
+            Strongly_type indata = new Strongly_type(_iconfiguration);
+            List<Employee> ls = indata.GetList();
+            foreach (var x in ls)
+                Console.WriteLine("{0}  {1}  {2}  {3}  {4}", x.Id, x.Name, x.Salary,x.Salary,x.Address);
+
+            Console.WriteLine("------------------------------------------------------------");
+           // int no = indata.Del(3);
+           //Console.WriteLine("deleted {0}", no);
+            Console.WriteLine("------------------------------------------------------------");
+            List<Employee> ls1 = indata.GetList();
+            foreach (var x in ls1)
+                Console.WriteLine("{0}  {1}  {2}  {3}  {4}", x.Id, x.Name, x.Salary, x.Salary, x.Address);
+            Console.WriteLine("------------------------------------------------------------");
+            Employee p1 = new Employee {Name = "Raviraj", Salary = "90000" ,Gender="Male" ,Address="lmc" };
+            //int a = indata.AddData(p1);
+           // Console.WriteLine("{0}", a);
+            Console.WriteLine("------------------------------------------------------------");
+            List<Employee> ls2 = indata.GetList();
+            foreach (var x in ls2)
+                Console.WriteLine("{0}  {1}  {2}  {3}  {4}", x.Id, x.Name, x.Salary, x.Salary, x.Address);
+            Console.WriteLine("------------------------------------------------------------");
+            Employee r = indata.search(1);
+            Console.WriteLine("{0} {1} {2} {3} {4}", r.Id, r.Name, r.Salary,r.Gender,r.Address); 
+
+
+
+        }
+    }
+
+   
+}
---
+++ Stronglt_type.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRDemo
+{
+    internal class Strongly_type
+    {
+        private string _connectionString;
+        public Strongly_type(IConfiguration iconfiguration)
+        {
+            _connectionString = iconfiguration.GetConnectionString("Default");
+        }
+        public SqlConnection getconnection()
+        {
+            SqlConnection sqlconn = new SqlConnection();
+            sqlconn.ConnectionString = _connectionString;
+            return sqlconn;
+        }
+        //search employee using id
+        public Employee search(int id)
+        {
+            SqlConnection sqlconn = null;
+            SqlCommand sqlcmd;
+            Employee p = null;
+            try
+            {
+                sqlconn = getconnection();
+                sqlconn.Open();
+                sqlcmd = new SqlCommand("select * from Employee where id=@pid", sqlconn);
+                sqlcmd.Parameters.AddWithValue("@pid", id);
+                SqlDataReader rd = sqlcmd.ExecuteReader();
+                if (rd.HasRows)
+                {
+                    while (rd.Read())
+                    {
+                        // int.TryParse("rd[Id"]", out r) ;
+                        p = new Employee();
+                        p.Id = Convert.ToInt32(rd["Id"]);
+                        p.Name = rd["Name"].ToString();
+                        p.Gender = rd["Gender"].ToString();
+                        p.Address= rd["Address"].ToString();
+                        p.Salary = rd["Salary"].ToString();
+                        break;
+                    }
+                }
+            }
+            catch (SqlException se)
+            { Console.WriteLine(se.Message); }
+            finally
+            {
+                sqlconn.Close();
+            }
+
+            return p;
+        }
+        //adding data to data base
+        public int AddData(Employee e)
+        {
+            SqlConnection sqlconn = null;
+            SqlCommand sqlcmd;
+            int record = 0;
+            try
+            {
+                sqlconn = getconnection();
+                sqlcmd = new SqlCommand("storedata", sqlconn);
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                sqlcmd.Parameters.Add("@pname", SqlDbType.NVarChar).Value = e.Name;
+                sqlcmd.Parameters.Add("@pgender", SqlDbType.NVarChar).Value = e.Gender;
+                sqlcmd.Parameters.Add("@paddress", SqlDbType.NVarChar).Value = e.Address;
+                sqlcmd.Parameters.Add("@psalary", SqlDbType.NVarChar).Value = e.Salary;
+
+                sqlconn.Open();
+                record = sqlcmd.ExecuteNonQuery();
+            }
+            catch (SqlException se)
+            { Console.WriteLine(se.Message); }
+            finally
+            {
+                sqlconn.Close();
+            }
+            return record;
+
+        }
+        //deleting data from data base using id
+        public int Del(int id)
+        {
+            SqlConnection sqlconn = null;
+            SqlCommand sqlcmd;
+            int no = 0;
+
+            using (sqlconn = getconnection())
+            {
+                try
+                {
+                    sqlconn.Open();
+                    sqlcmd = new SqlCommand("delete from Employee where id=@pid", sqlconn);
+                    sqlcmd.Parameters.AddWithValue("@pid", id);
+                    no = sqlcmd.ExecuteNonQuery();
+                }
+                catch (SqlException se)
+                { Console.WriteLine(se.Message); }
+            }
+
+
+            return no;
+        }
+        //search all data from data base and store in list
+        public List<Employee> GetList()
+        {
+            var listEmployee = new List<Employee>();
+            try
+            {
+                using (SqlConnection con = new SqlConnection(_connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("SP_EMP_GET_LIST", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        listEmployee.Add(new Employee
+                        {
+                            Id = Convert.ToInt32(rdr["Id"]),
+                            Name = rdr["Name"].ToString(),
+                            Salary = rdr["Salary"].ToString(),
+                            Gender= rdr["Gender"].ToString(),
+                            Address= rdr["Address"].ToString(),
+
+                        });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return listEmployee;
+        }
+    }
+}
+
+    
